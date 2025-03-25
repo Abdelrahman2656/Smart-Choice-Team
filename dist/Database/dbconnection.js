a035e38a4b36da1d0d8b554fb8eb76a71efb9bc1
Abdelrahman2656 <a.elmonged870@gmail.com>
2025-03-25 22:39:02 +0200

    add

--- dist/Database/dbconnection.js
+++ dist/Database/dbconnection.js
@@ -1,12 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbconnection = dbconnection;
-const mongoose_1 = __importDefault(require("mongoose"));
+const mongoose_1 = require("mongoose");
 async function dbconnection() {
-    await mongoose_1.default.connect(process.env.DATABASE_DB).then(() => {
+    await (0, mongoose_1.connect)(process.env.DATABASE_DB).then(() => {
         console.log('db connected successfully');
     }).catch(() => {
         console.log('failed to connected to db');
