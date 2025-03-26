6914110a75df5d55711e7a52fadc966e01e1d905
Abdelrahman2656 <a.elmonged870@gmail.com>
2025-03-26 02:01:49 +0200

    add

--- Src/bootstrap.ts
+++ Src/bootstrap.ts
@@ -5,24 +5,24 @@ import path from "path";
 import { globalErrorHandling } from "./Middleware/asyncHandler";
 import { productRouter, userRouter } from "./Modules";
 import { dbconnection } from '../Database/dbconnection';
-// import { startSeeding } from '../Database/seed';
+import { startSeeding } from '../Database/seed';
 export const bootstrap = async(
   app: Application,
   express: typeof import("express")
 ) => {
+  dotenv.config({ path: path.resolve("./.env") });
   //-----------------------------------------------DataBase Connection------------------------------------------------------------
   console.log("⏳LOADING");
- await dbconnection()
+  dbconnection()
  console.log("✅✌️ تم الاتصال بقاعدة البيانات بنجاح!");
   //-----------------------------------------------parse------------------------------------------------------------
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  dotenv.config({ path: path.resolve("./.env") });
   app.use(cors({
-   origin: '*', 
- }));
-  // startSeeding()
- //----------------------------------------------- Use the auth router------------------------------------------------------------
+    origin: '*', 
+  }));
+  startSeeding()
+  //----------------------------------------------- Use the auth router------------------------------------------------------------
 
  app.use('/api/v1',userRouter);
  app.use("/api/v1/products", productRouter);
--- dist/Src/bootstrap.js
+++ dist/Src/bootstrap.js
@@ -10,20 +10,20 @@ const path_1 = __importDefault(require("path"));
 const asyncHandler_1 = require("./Middleware/asyncHandler");
 const Modules_1 = require("./Modules");
 const dbconnection_1 = require("../Database/dbconnection");
-// import { startSeeding } from '../Database/seed';
+const seed_1 = require("../Database/seed");
 const bootstrap = async (app, express) => {
+    dotenv_1.default.config({ path: path_1.default.resolve("./.env") });
     //-----------------------------------------------DataBase Connection------------------------------------------------------------
     console.log("⏳LOADING");
-    await (0, dbconnection_1.dbconnection)();
+    (0, dbconnection_1.dbconnection)();
     console.log("✅✌️ تم الاتصال بقاعدة البيانات بنجاح!");
     //-----------------------------------------------parse------------------------------------------------------------
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    dotenv_1.default.config({ path: path_1.default.resolve("./.env") });
     app.use((0, cors_1.default)({
         origin: '*',
     }));
-    // startSeeding()
+    (0, seed_1.startSeeding)();
     //----------------------------------------------- Use the auth router------------------------------------------------------------
     app.use('/api/v1', Modules_1.userRouter);
     app.use("/api/v1/products", Modules_1.productRouter);
