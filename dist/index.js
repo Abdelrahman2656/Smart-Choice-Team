fb74d03586f23d49e46a9970138db3fb857d2156
Abdelrahman2656 <a.elmonged870@gmail.com>
2025-04-27 19:33:35 +0300

    add

--- dist/index.js
+++ dist/index.js
@@ -7,9 +7,7 @@ const app_controller_1 = __importDefault(require("./Src/app.controller"));
 const express = require('express');
 const port = process.env.PORT || 3001;
 const app = express();
-(async () => {
-    await (0, app_controller_1.default)(app, express);
-    app.get('/', (req, res) => res.send('Hello World In My Smart Choice App'));
-})();
+(0, app_controller_1.default)(app, express);
+app.get('/', (req, res) => res.send('Hello World In My Smart Choice App'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 exports.default = app;
--- index.ts
+++ index.ts
@@ -11,11 +11,11 @@ const app = express();
 
 
 
-(async () => {
-    await bootstrap(app, express);
+
+     bootstrap(app, express);
   
     app.get('/', (req: AppRequest, res: AppResponse) => res.send('Hello World In My Smart Choice App'));
-  })();
+ 
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 export default app; 
